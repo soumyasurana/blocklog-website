526290fec966db82494ee311697ff75e11502c6e
Soumya <soumyasurana2005@gmail.com>
2026-07-02 14:01:06 +0530

    chore: remove node_modules from version control

--- node_modules/posthog-js/dist/src/config.d.ts
+++
@@ -1,8 +0,0 @@
-declare const Config: {
-    DEBUG: boolean;
-    LIB_VERSION: string;
-    LIB_NAME: string;
-    /** The actual JS SDK version, unaffected by _overrideSDKInfo. Used for the `ver` request param. */
-    JS_SDK_VERSION: string;
-};
-export default Config;
